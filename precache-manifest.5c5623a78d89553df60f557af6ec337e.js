179e3fadedbd69a5de89a4ba27727bd0f2587a18
Karthik Arumugam <chivalrykarthik@gmail.com>
2020-12-13 13:07:14 +0530

    Updates

--- precache-manifest.5c5623a78d89553df60f557af6ec337e.js
+++ precache-manifest.5c5623a78d89553df60f557af6ec337e.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5e8091a6827130e27678a2ec60797bd6",
+    "revision": "521e9618a0d22f378281e6ac2968a003",
     "url": "/tax-planner/index.html"
   },
   {
-    "revision": "2271bb20bb528a46d079",
+    "revision": "e493dafd85696cfbb8e2",
     "url": "/tax-planner/static/css/main.5f361e03.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/tax-planner/static/js/2.6a703402.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "2271bb20bb528a46d079",
-    "url": "/tax-planner/static/js/main.204e7c92.chunk.js"
+    "revision": "e493dafd85696cfbb8e2",
+    "url": "/tax-planner/static/js/main.c61b655e.chunk.js"
   },
   {
     "revision": "7c38556e7300efe5323c",
